24c7b25a714bf667de469a2767ea2f47d605e3ca
Rajan kumar <krcpr007@gmail.com>
2023-03-23 11:28:19 +0530

    success header

--- src/Pages/Screen2.js
+++ src/Pages/Screen2.js
@@ -1,19 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import CharacterComp from '../Components/CharacterComp'
 
 function Screen2({ inputString }) {
-  const [result, setResult] = useState('')
+  const [result, setResult] = useState(inputString) // for displaying the result
+  const [leftString, setLeftString] = useState('') // for displaying Resultant character card 
+  const[unique,setUnique] = useState(false) //for checking the string contain unique characters 
+  
+  // Returns true if the input string s does not contain any duplicate characters.
+  function hasUniqueChars(s) {
+    let seen = new Set();
+    for (let i = 0; i < s.length; i++) {
+      let c = s.charAt(i);
+      if (seen.has(c)) {
+        return false;
+      }
+      seen.add(c);
+    }
+    return true;
+  }
+
+  useEffect(()=>{
+    if(hasUniqueChars(result)){
+    //here set it has unique character 
+    setUnique(true);
+    }
+  },[result])
+
+  //function to delete character 
   const handleDeleteCharacter = (char, index) => {
     try {
-      let result = "";
-      for (let i = 0; i < inputString.length; i++) {
-        if (i === index || inputString.charAt(i) !== char) {
-          result += inputString.charAt(i);
+      let newResult = "";
+      for (let i = 0; i < result.length; i++) {
+        if (i === index || result.charAt(i) !== char) {
+          newResult += result.charAt(i);
         }
       }
-      setResult(result)
+      setResult(newResult)
+      setLeftString(newResult)
       toast.success(`Successfully removed '${char}'`)
     } catch (error) {
       console.log(error)
@@ -32,6 +57,15 @@ function Screen2({ inputString }) {
   return (
     <>
       <section>
+        {unique && <>
+        <div className='bg-green-400'>
+            <p className=' px-3 lg:px-5 py-5 text-xl text-black'>Success! There are no Duplicate characters in the string.</p>
+        </div>
+        </>}
+        <div className='px-3 lg:px-5 bg-rose-100 py-5 text-2xl'>
+         <p>Original String: <span className='text-red-400'>{inputString}</span></p>
+         <p>Resultant string: <span className='text-green-500'>{result}</span></p>
+        </div>
         <div className='px-3 lg:px-5 flex flex-wrap '>
           {[...inputString]?.map((char, i) => {
             return <CharacterComp character={char} key={i} index={i} handleDeleteCharacter={handleDeleteCharacter} />
@@ -41,7 +75,7 @@ function Screen2({ inputString }) {
           <h1 className='text-lg md:text-2xl text-center '>New Resultant String will show below</h1>
         </div>
         <div className='flex flex-wrap px-3 lg:px-5'>
-          {[...result]?.map((char, i) => {
+          {[...leftString]?.map((char, i) => {
             return <div key={i} className="bg-green-500 m-1 w-20 flex justify-center rounded"> <h1 className='text-white text-5xl font-mono pb-2'>{char}</h1></div>
           })}
         </div>
